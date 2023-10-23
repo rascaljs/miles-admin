21af57e2480bf3f152950bfa0f8f72036a75087f
qi yuhang <qiyuhang@thinkerx.com>
2023-10-23 17:36:18 +0800

    build(deps): 添加依赖 naive-ui

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>项目初始化</div>
-  <HelloWorld msg="Vite + Vue" />
+  <n-space>
+    <n-button>Default</n-button>
+    <n-button type="tertiary">Tertiary</n-button>
+    <n-button type="primary">Primary</n-button>
+    <n-button type="info">Info</n-button>
+    <n-button type="success">Success</n-button>
+    <n-button type="warning">Warning</n-button>
+    <n-button type="error">Error</n-button>
+  </n-space>
 </template>
 
 <style scoped></style>
